35b35170d51e7cd5737c0b3ae10849ae2b375418
Beau Simensen <beau@dflydev.com>
2012-11-27 08:08:30 -0800

    Updated publish script.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-vendor/bin/sculpin generate --env=prod
-vendor/bin/sculpin assets:install --env=prod output_prod
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+vendor/bin/sculpin generate --env=prod || ( echo "Could not generate the site" && exit )
+vendor/bin/sculpin assets:install --env=prod output_prod || ( echo "Could not install assets for the site" && exit )
+rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html || ( echo "Could not publish the site" && exit )
