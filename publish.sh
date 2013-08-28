3f1c43daa57e9acfeacd122d171d37bf62b0fd47
Beau Simensen <beau@dflydev.com>
2013-08-28 14:26:33 -0500

    More robust errors.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-vendor/bin/sculpin generate --env=prod || ( echo "Could not generate the site" && exit )
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html || ( echo "Could not publish the site" && exit )
+# Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
+# was downloaded and placed in this directory instead of sculpin having been
+# installed globally.
+
+sculpin generate --env=prod
+if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
+
+rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
